7e258d5126b3b76ac8a62c846e32b7c83888084d
emily baier <emilyrbaier@gmail.com>
2019-01-16 20:42:08 -0800

    made something that a user can interact with, will print to console

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
-var sum = 3 + 4;
-console.log('hello console!', sum);
+var x = 15;
+
+if(x > 12) {
+    console.log(x);
+}
+
+else {
+    console.log('yo too small');
+}
+
+// intialize
+// condition
+// post loop action
+
+for(var i = 0; i < x; i++) {
+
+    if(i % 2 === 0) {
+        console.log(i);
+    }
+    else {
+        sayHello();
+    }
+}
+
+function sayHello() {
+    console.log('hi');
+}
+// eslint-disable-next-line
+function evenOrOdd() {
+    var input = document.getElementById('number');
+
+
+    var y = input.value;
+    if(y % 2 === 0) {
+        console.log('even');
+    }
+    else {
+        console.log('odd');
+    }
+}
